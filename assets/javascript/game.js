b5954eee49fc85e81bb3816a5c6df039bb62beb6
Noel Parisi <NoelParisi@calvisitor-10-105-178-49.calvisitor.1918.berkeley.edu>
2017-03-02 20:58:56 -0800

    adds console.logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,12 @@
 // create an object containing an array of my questions/ choices/ and functions
 var time = 31;
-var answerTime = 6;
 var currentQuestion;
 var decrementTime;
 var waitToChoose;
+var incorrectAnswers = 0;
+var correctAnswers = 0;
+var value;
+var currentQuestionIndex;
 
 var game = {
 
@@ -14,17 +17,17 @@ var game = {
             answerChoices: [
 
                 {
-                    text: "choice one",
+                    text: "choice for question one",
                     value: true
                 },
 
                 {
-                    text: "choice two",
+                    text: "choice for question one",
                     value: false
                 },
 
                 {
-                    text: "choice three",
+                    text: "choice for question one",
                     value: false
                 }
             ]
@@ -35,17 +38,17 @@ var game = {
             answerChoices: [
 
                 {
-                    text: "choice one",
+                    text: "choice for question two",
                     value: false
                 },
 
                 {
-                    text: "choice two",
+                    text: "choice for question two",
                     value: true
                 },
 
                 {
-                    text: "choice three",
+                    text: "choice for question two",
                     value: false
                 }
             ]
@@ -56,17 +59,17 @@ var game = {
             answerChoices: [
 
                 {
-                    text: "choice one",
+                    text: "choice for question three",
                     value: false
                 },
 
                 {
-                    text: "choice two",
+                    text: "choice for question three",
                     value: false
                 },
 
                 {
-                    text: "choice three",
+                    text: "choice for question three",
                     value: true
                 }
             ]
@@ -77,17 +80,17 @@ var game = {
             answerChoices: [
 
                 {
-                    text: "choice one",
+                    text: "choice for question four",
                     value: false
                 },
 
                 {
-                    text: "choice two",
+                    text: "choice for question four",
                     value: false
                 },
 
                 {
-                    text: "choice three",
+                    text: "choice for question four",
                     value: true
                 }
             ]
@@ -96,6 +99,8 @@ var game = {
     ],
 
     questionTimer: function questionTimer(obj) {
+
+        console.log("The questionTimer is running!")
 
         decrementTime = setInterval(function() {
             time -= 1;
@@ -114,6 +119,7 @@ var game = {
 
 var displayTimer = function displayTimer() {
     // display question timer
+    console.log("The displayTimer function is running");
     game.questionTimer(game.questions[0]);
     time = 31;
 
@@ -121,48 +127,77 @@ var displayTimer = function displayTimer() {
 
 var clearHTMLdivs = function clearHTMLdivs() {
 
+    console.log("The clearHTMLdivs function is running");
+
     $('.correct').empty();
     $('.incorrect').empty();
 
 }
 
+//when all the questions in the array have been asked it's time to end the game!
 var gameOver = function gameOver() {
 
+    console.log("The gameOver function is running!")
+    //I want to stop the timer and clear the div
     $('.question-timer').empty();
+    clearInterval(decrementTime);
+
+    // I want to display a game over sentence
     $('.question').html('<strong> GAME OVER</strong>');
+
+    // no more anser choices should show!
     $('.answerChoices').empty();
+    // empty the correct and incorrect divs
     $('.correct').empty();
     $('.incorrect').empty();
-    clearInterval(decrementTime);
+
+    //display their score!
+    $('.score').html("<p>You got this many wrong: </p>" + incorrectAnswers + "<p>You got this many correct: </p>" + correctAnswers);
 }
 
+var incrementScore = function(event) {
+
+    console.log("The incrementScore function is running!")
+
+    // if they clicked on the right answer increment their score
+    if (event.target.attributes[1].nodeValue === "true"){
+
+        correctAnswers ++;
+        console.log("The number of correct answers: " + correctAnswers); 
+
+    // if they got it wrong do the same thing!
+    } else {
+
+        incorrectAnswers ++;
+        console.log("The number of incorrect answers: " + incorrectAnswers);
+
+
+    } 
+
+}
+
+// main function of the game!
 var chooseRandomQuestion = function chooseRandomQuestion() {
 
+    console.log("The chooseRandomQuestion function is running!")
+
+    // if set timer variable is true, clear it so that it doesn't load every 3 secs
     if (waitToChoose) {
         clearTimeout(waitToChoose);
     }
 
-    var currentQuestionIndex = Math.floor(Math.random() * game.questions.length);
+    // choose random question from the game.questions object
 
-    var currentQuestion = game.questions[currentQuestionIndex]
+    currentQuestionIndex = Math.floor(Math.random() * game.questions.length);
 
-    game.questions.splice(currentQuestionIndex, 1);
-
-    console.log(game.questions);
-
-    if (game.questions.length === 0) {
-
-        // when there are no more question to ask exit function and stop game
-        clearTimeout(waitToChoose);
-        var stopGame = setTimeout(gameOver(),4000);
-    }
+    currentQuestion = game.questions[currentQuestionIndex];
 
     // display question  
 
     $('.question').html('<p id="question">' + currentQuestion.question + '</p>');
 
 
-    // display answer choices 
+    // assign value to each answer choice so I can access it later to increment score 
 
     for (let i = 0; i < currentQuestion.answerChoices.length; i++) {
 
@@ -181,6 +216,8 @@ var chooseRandomQuestion = function chooseRandomQuestion() {
 
     $('.answerChoices').on('click', function(event) {
 
+        console.log("is this running");
+
         $('.question-timer').empty();
 
         $('.answerChoices').empty();
@@ -193,17 +230,22 @@ var chooseRandomQuestion = function chooseRandomQuestion() {
             chooseRandomQuestion();
             displayTimer();
             clearHTMLdivs();
-        }, 5000);
+        }, 3000);
 
         if ($(event.target).attr('value') === 'true') {
 
             $('.correct').html("<strong> Correct </strong");
+
+            // correctAnswers ++;
+            // console.log("The number of correct answers: " + correctAnswers);
 
             // if wrong answer
 
         } else {
 
             $('.incorrect').html("<strong> Incorrect </strong");
+            // incorrectAnswers ++;
+            // console.log("The number of incorrect answers: "+ incorrectAnswers);
         }
 
     })
@@ -211,6 +253,7 @@ var chooseRandomQuestion = function chooseRandomQuestion() {
 }
 
 
+// console.log(game.questions[0].answerChoices[0].value);
 // when button is clicked start the game
 $('button').on('click', function(event) {
 
@@ -218,5 +261,21 @@ $('button').on('click', function(event) {
     displayTimer();
 });
 
+$('.answerChoices').on('click', function(){
 
+    incrementScore(event);
+    //take the qyestion that was just asked out of the array
+    game.questions.splice(currentQuestionIndex, 1);
+    console.log(currentQuestionIndex);
+
+    if (game.questions.length === 0) {
+
+        // when there are no more question to ask exit function and stop game
+        clearTimeout(waitToChoose);
+        var stopGame = setTimeout(gameOver(),4000);
+        console.log("how is this doing?");
+    }
+
+
+});
 
